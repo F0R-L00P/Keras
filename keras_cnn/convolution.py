1c25c5152736634dd93aade21705318b4b2a1764
F0R-L00P <67719682+F0R-L00P@users.noreply.github.com>
2023-04-17 13:55:01 -0400

    20230417

--- keras_cnn/convolution.py
+++ keras_cnn/convolution.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 
+
 def generate_model(input_image_shape):
     model = tf.keras.Sequential([
         # first convolutional layer
         # result is to learn 32 features/results
-        tf.keras.layers.Conv2D(32, kernel_size=3, activation='relu', input_shape=input_image_shape),
+        tf.keras.layers.Conv2D(
+            32, kernel_size=3, activation='relu', input_shape=input_image_shape),
         tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
 
         # second convolutional layer
@@ -20,10 +22,12 @@ def generate_model(input_image_shape):
 
     # Compile the model
     model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
                   metrics=['accuracy'])
 
     return model
+
 
 n = 100  # Number of images in the batch
 input_shape = (32, 32, 3)  # Shape of the tensor (height, width, channels)
